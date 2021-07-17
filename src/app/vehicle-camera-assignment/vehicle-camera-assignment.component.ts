39200f0dfa719df12485a9f8bb895fe948451991
barrytravis <barrytravis13@gmail.com>
2021-07-16 22:40:55 -0500

    4 files modified

--- src/app/vehicle-camera-assignment/vehicle-camera-assignment.component.ts
+++ src/app/vehicle-camera-assignment/vehicle-camera-assignment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { AssignmentResponse } from '../models/assignment.model';
 import { DataService } from '../services/data.service';
@@ -9,20 +10,16 @@ import { DataService } from '../services/data.service';
   styleUrls: ['./vehicle-camera-assignment.component.css']
 })
 export class VehicleCameraAssignmentComponent implements OnInit {
+  
   constructor(private data: DataService) {}
 
+  assignments: AssignmentResponse[];
+
   ngOnInit() {
-    // this.getAssignments().subscribe(
-    //   next => {
-    //   },
-    //   error => {
-    //   },
-    //   complete => {
-    //   }
-    // )
+    this.assignments = this.getAssignments().subscribe();
   }
 
-  getAssignments() {
+  getAssignments(): Observable<AssignmentResponse> {
     return this.data.get<AssignmentResponse>('assignments');
   }
 
