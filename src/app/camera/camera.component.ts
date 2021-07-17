d1cd2a8400d10aef408c5176a828751a3ce0ae6c
barrytravis <barrytravis13@gmail.com>
2021-07-16 21:39:31 -0500

    2 files modified

--- src/app/camera/camera.component.ts
+++ src/app/camera/camera.component.ts
@@ -9,32 +9,27 @@ import { DataService } from '../services/data.service';
   styleUrls: ['./camera.component.css']
 })
 export class CameraComponent implements OnInit {
-cameras: Camera[];
+  cameras: Camera[];
 
-  constructor(
-    private data: DataService
-  ) {}
+  constructor(private data: DataService) {}
 
   ngOnInit() {
-    this.getCameras.subscribe(
-      x => this.cameras = x
-    );
+    this.getCameras().subscribe(x => (this.cameras = x));
   }
 
   getCameras(): Observable<Camera[]> {
-    return this.data.get<Camera[]>("cameras");
+    return this.data.get<Camera[]>('cameras');
   }
 
   getCameraById(id: number): Observable<Camera> {
-    return this.data.get<Camera>("cameras/:id", { id: id });
+    return this.data.get<Camera>('cameras/:id', { id: id });
   }
 
   addCamera() {
     this.data.post(
-      "cameras/:id",
+      'cameras/:id',
       { id: 1 },
-      { name: "camera 3", vehicleId: 0 }
-    )
+      { name: 'camera 3', vehicleId: 0 }
+    );
   }
-
 }
