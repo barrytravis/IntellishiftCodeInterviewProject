59c765b19e36eaf3abdd2db42e858b8b769fe7d8
barrytravis <barrytravis13@gmail.com>
2021-07-20 18:34:24 -0500

    3 files added

---
+++ src/app/+store/reducers/index.ts
@@ -0,0 +1,5 @@
+import * as CameraReducers from '../camera.reducers';
+import * as VehicleReducers from '../vehicle.reducers';
+import * as AssignmentReducers from './assignment.reducers';
+
+export { CameraReducers , VehicleReducers , AssignmentReducers };
---
+++ src/app/+store/selectors/index.ts
@@ -0,0 +1,5 @@
+import * as CameraSelectors from '../camera.selectors';
+import * as VehicleSelectors from '../vehicle.selectors';
+import * as AssignmentSelectors from './assignment.selectors';
+
+export { CameraSelectors , VehicleSelectors , AssignmentSelectors };
