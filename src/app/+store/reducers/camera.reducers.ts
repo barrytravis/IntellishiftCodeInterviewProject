6899aed9feb4bdad17826ab25f7ccea3c2df87f7
barrytravis <barrytravis13@gmail.com>
2021-07-20 19:17:54 -0500

    1 file modified

--- src/app/+store/reducers/camera.reducers.ts
+++ src/app/+store/reducers/camera.reducers.ts
@@ -1,11 +1,21 @@
-import { EntityState, createEntityAdapter } from '@ngrx/entity';
+import { EntityState } from '@ngrx/entity';
 import { createReducer, on } from '@ngrx/store';
 import * as CameraActions from '../camera.actions';
 import { Camera } from '../../models/camera.model';
 
-export const reducer  = createReducer(
-  [],
-  on(CameraActions.createCamera, (state, { camera }) => {
+export interface State {
+  cameras: Array<Camera>;
+}
 
-  })
+export const initialState: State = { cameras: [] };
+
+export const cameraReducer = createReducer(
+  initialState,
+  on(CameraActions.setScores, (state, { camera }) => ({
+    cameras: ...state.cameras.push(camera)
+  }))
 );
+
+export function reducer(state: State | undefined, action: Action) {
+  return cameraReducer(state, action);
+}
