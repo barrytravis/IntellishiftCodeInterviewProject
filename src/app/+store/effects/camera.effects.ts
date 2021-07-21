d18e44ac3841ed0b773787d1f93aebc52a7872d6
barrytravis <barrytravis13@gmail.com>
2021-07-20 22:20:48 -0500

    1 file modified

--- src/app/+store/effects/camera.effects.ts
+++ src/app/+store/effects/camera.effects.ts
@@ -1,29 +1,24 @@
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { map, mergeMap, catchError, switchMap } from 'rxjs/operators';
+import { map, catchError, switchMap } from 'rxjs/operators';
 import { DataService } from '../../services/data.service';
 import { Injectable } from '@angular/core';
 import { CameraActions } from '../../+store/actions';
 import { Camera } from '../../models/camera.model';
-import { Action } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { EMPTY } from 'rxjs';
 
 @Injectable()
 export class CameraEffects {
-  constructor(
-    private dataService: DataService, private actions: Actions) {}
+  constructor(private dataService: DataService, private actions$: Actions) {}
 
-  loadCameras = createEffect(() => {
-    this.actions.pipe(
-      ofType(
-        CameraActions.loadCameras,
-        switchMap(() =>
-          this.dataService
-            .get<Camera[]>('cameras')
-            .pipe(map(camereas => CameraActions.loadCamerasSuccess(camereas)))
+  loadCameras = createEffect(() =>
+    this.actions$.pipe(
+      ofType(CameraActions.loadCameras),
+      switchMap(() =>
+        this.dataService.get<Camera[]>('cameras').pipe(
+          map(cameras => CameraActions.loadCamerasSuccess({ cameras })),
+          catchError(() => EMPTY)
         )
       )
-    );
-
-    return new Observable<Action>();
-  });
+    )
+  );
 }
